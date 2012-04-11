d06a31ca8c827c11699ebe21825f6d936c2c5348
Iñaki Baz Castillo <ibc@aliax.net>
2012-04-11 17:38:59 +0200

    Removed useless "callbacks" in Resolver class.

--- lib/em-udns/resolver.rb
+++ lib/em-udns/resolver.rb
@@ -7,27 +7,11 @@ module EventMachine::Udns
       dns_open
     end
 
-    def callback &block
-      @on_success_block = block
-    end
-
-    def errback &block
-      @on_error_block = block
-    end
-
 
     private
-    
+
     def set_timer(timeout)
       @timer = EM::Timer.new(timeout) { timeouts }
-    end
-    
-    def do_success result
-      @on_success_block && @on_success_block.call(result)
-    end
-
-    def do_error error
-      @on_error_block && @on_error_block.call(error)
     end
   end
 
